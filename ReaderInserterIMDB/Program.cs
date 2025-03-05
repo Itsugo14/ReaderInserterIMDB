13591311c2eb8c5175eac8365d7c848a5d93640c
move <move@zealand.dk>
2025-03-05 13:41:21 +0100

    added prepared

--- ReaderInserterIMDB/Program.cs
+++ ReaderInserterIMDB/Program.cs
@@ -1,72 +1,102 @@
 ﻿using Microsoft.Data.SqlClient;
+using ReaderInserterIMDB;
+using System.Diagnostics;
 
 using (SqlConnection sqlConn = new SqlConnection("server=localhost;Database=IMDB;" +
                "Integrated security=True;TrustServerCertificate=True"))
 {
     sqlConn.Open();
-    foreach (string line in File.ReadLines("C:/temp/title.basics (1).tsv").Skip(1).Take(10000))
-    {
-        string[] splitLine = line.Split("\t");
-        if (splitLine.Length != 9)
-        {
-            Console.WriteLine("Linjen har ikke 9 kolonner: " + line);
-        }
-        else
-        {
-            string titleType = splitLine[1];
-            SqlCommand sqlComm = new SqlCommand("EXEC [dbo].[TitleTypeGetInsertID] @NewTiteType = '"
-                + titleType + "'", sqlConn);
-            SqlDataReader reader = sqlComm.ExecuteReader();
-            if (reader.Read())
-            {
-                int titleTypeId = (int)reader["Id"];
-                Console.WriteLine("Indlæst Titletypeid: " + titleTypeId);
-                reader.Close();
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
 
-                string tconst = splitLine[0];
-                string primaryTitle = splitLine[2];
-                string originalTitle = splitLine[3];
-                bool? isAdult = CheckBit(splitLine[4]);
-                int? startYear = CheckInt(splitLine[5]);
-                int? endYear = CheckInt(splitLine[6]);
-                int? runtimeMinutes = CheckInt(splitLine[7]);
+    SqlTransaction myTrans = sqlConn.BeginTransaction();
+    int counter = 0;
+
+    try
+    {
+        //IInserter inserter = new NormalInserter(sqlConn, myTrans);
+        IInserter inserter = new PreparedInserter(sqlConn, myTrans);
+        foreach (string line in File.ReadLines("C:/temp/title.basics (1).tsv").Skip(1).Take(10000))
+        {
+            counter++;
+            string[] splitLine = line.Split("\t");
+            if (splitLine.Length != 9)
+            {
+                Console.WriteLine("Linjen har ikke 9 kolonner: " + line);
             }
             else
             {
-                Console.WriteLine("Kunne ikke få id for titletype: " + titleType);
-                reader.Close();
-            }
+                string titleType = splitLine[1];
+                SqlCommand sqlComm = new SqlCommand("EXEC [dbo].[TitleTypeGetInsertID] @NewTiteType = '"
+                    + titleType + "'", sqlConn, myTrans);
+                SqlDataReader reader = sqlComm.ExecuteReader();
+                if (reader.Read())
+                {
+                    string titleTypeId = reader["Id"].ToString();
+                    //Console.WriteLine("Indlæst Titletypeid: " + titleTypeId);
+                    reader.Close();
 
+                    string tconst = splitLine[0];
+                    string primaryTitle = splitLine[2].Replace("'", "''");
+                    string originalTitle = splitLine[3].Replace("'", "''");
+                    string isAdult = CheckBit(splitLine[4]);
+                    string startYear = CheckInt(splitLine[5]);
+                    string endYear = CheckInt(splitLine[6]);
+                    string runtimeMinutes = CheckInt(splitLine[7]);
+
+                    Title newTitle = new Title()
+                    {
+                        tconst = tconst,
+                        titleTypeId = titleTypeId,
+                        primaryTitle = primaryTitle,
+                        originalTitle = originalTitle,
+                        isAdult = isAdult,
+                        startYear = startYear,
+                        endYear = endYear,
+                        runtimeMinutes = runtimeMinutes
+                    };
+                    inserter.InsertTitle(newTitle);
+                }
+                else
+                {
+                    Console.WriteLine("Kunne ikke få id for titletype: " + titleType);
+                    reader.Close();
+                }
+            }
         }
     }
+    catch (SqlException ex)
+    {
+        Console.WriteLine("linje nummer: " + counter + "\r\n" + ex.ToString());
+    }
+    stopwatch.Stop();
+    Console.WriteLine("Elapsed milli: " + stopwatch.ElapsedMilliseconds);
+
+    myTrans.Rollback();
 }
 
-bool? CheckBit(string bit)
+string CheckBit(string bit)
 {
-    if (bit == "1")
+    if (bit == "1" || bit == "0")
     {
-        return true;
-    }
-    else if (bit == "0")
-    {
-        return false;
+        return bit;
     }
     else if (bit.ToLower() == "\\n")
     {
-        return null;
+        return "NULL";
     }
     throw new Exception("Hvordan fanden ser dit bit ud!!!");
 }
 
-int? CheckInt(string input)
+string CheckInt(string input)
 {
     if (int.TryParse(input, out int result))
     {
-        return result;
+        return input;
     }
     else if (input.ToLower() == "\\n")
     {
-        return null;
+        return "NULL";
     }
-    throw new Exception("Hvordan fanden ser dit bit ud!!!");
+    throw new Exception("Hvordan fanden ser din int ud!!!");
 }
